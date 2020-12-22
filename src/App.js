411ea2e1af8edb9286d41017fc61fef38f30eed8
Robert Gifford <timeonator@gmail.com>
2020-12-22 01:31:59 -0800

    Cleaned up code moved logon.js to login.js move old Login.js to Login-html-form.js Adjust remaining files to new names

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 
 import './App.css';
-import Counter from './Counter';
-import Logon from './logon'; /* logon rendered with components */
-import Login from './Login'; /* login rendered with html */
 import MLogin from './mLogin' /* Modal Login */
 
 
@@ -10,9 +7,6 @@ function App() {
   return (
     <div className="App">
     <MLogin />
-      <Login />
-      <Counter />
-      <Logon />
     </div>
   );
 }

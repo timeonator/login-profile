83f56d75fbbca187a8d25797eb60c9d385bca67a
Robert Gifford <timeonator@gmail.com>
2020-12-23 09:47:36 -0800

    profile: profile dialog form mProfile: Modal for profile dialog App: added new profile modal element for testing.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import MLogin from './mLogin' /* Modal Login */
 import MRegister from './mRegister'; /* Modal Register Dialog */
+import MProfile from './mProfile';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
     <MLogin />
     <MRegister />
+    <MProfile />
     </div>
   );
 }
---
+++ src/mProfile.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Profile from './profile';;
+
+function MProfile() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => {
+        setShow(true);
+        console.log("show:", show);
+    }
+
+    return(
+        <>
+        <Button variant="primary" onClick={handleShow}>
+            Edit Profile
+       </Button>
+    
+          <Modal
+            show={show}
+            onHide={handleClose}
+            backdrop="static"
+            keyboard={false}
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Edit Profile</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Profile />
+            </Modal.Body>
+          </Modal>
+        </>
+    )};
+ 
+    export default MProfile;

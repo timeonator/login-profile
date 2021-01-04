234f22604aa29dac14680943f37067a65b867e0e
Robert Gifford <timeonator@gmail.com>
2021-01-04 11:29:34 -0800

    basicRoute: adds navigation and routing profile: removed unused import of useState App: import new BasicRoute component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import BasicRoute from './basicRoute';
 import './App.css';
 import MLogin from './mLogin' /* Modal Login */
 import MRegister from './mRegister'; /* Modal Register Dialog */
@@ -8,6 +8,7 @@ import MProfile from './mProfile';
 function App() {
   return (
     <div className="App">
+    <BasicRoute />
     <MLogin />
     <MRegister />
     <MProfile />
---
+++ src/basicRoute.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import MProfile from './mProfile';
+
+export default function BasicRoute() {
+  return (
+
+    <Router>
+      <div>
+        <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Example</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className = "d-flex justify-content-end" p2>
+                <Nav.Link eventKey="home" href="/">Home</Nav.Link>
+                <Nav.Link eventKey="about" href="/About">About</Nav.Link>
+                <Nav.Link eventKey="users" href="/Dashboard">Dashboard</Nav.Link>
+            </Nav>
+            <MProfile />
+        </Navbar.Collapse>
+        </Navbar>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}

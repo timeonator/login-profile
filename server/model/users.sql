8a3a8eea5366805861cf052edd97556139ddd5a6
timeonator <timeonator@gmail.com>
2021-01-20 14:40:43 -0800

    users.sql: code to create empty users table

---
+++ server/model/users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `ip_name` varchar(45) DEFAULT NULL,
+  `role` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `idx_users_email` (`email`),
+  KEY `idx_users_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=1014 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

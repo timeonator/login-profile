9be3061a08c63e5f86325753c3650bbd900fa9c7
timeonator <timeonator@gmail.com>
2021-01-21 22:34:50 -0800

    model/users.sql: added password column removed ip_name column

--- server/model/users.sql
+++ server/model/users.sql
@@ -4,10 +4,11 @@ CREATE TABLE `users` (
   `last_name` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
   `username` varchar(45) DEFAULT NULL,
-  `ip_name` varchar(45) DEFAULT NULL,
+  `password` varchar(60) DEFAULT NULL,
   `role` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `idx_users_email` (`email`),
   KEY `idx_users_username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1014 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+alter table `users` rename column `ip_name` to `password`;
